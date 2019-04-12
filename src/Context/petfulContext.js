7e384a2a2fe3d792b69ddd8c747856cb5700f459
Robin Khiv <robinkhiv@gmail.com>
2019-04-12 13:50:18 -0700

    default context values

--- src/Context/petfulContext.js
+++ src/Context/petfulContext.js
@@ -10,64 +10,13 @@ export default PetfulContext;
 export class PetfulProvider extends Component {
     state={
         Dogs: {
-            first: [{
-                id: 1,
-                imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-                imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-                name: 'Fluffy1',
-                sex: 'Female',
-                age: 2,
-                breed: 'Bengal',
-                story: 'Thrown on the street'
-                }],
-            display: [{
-                id:1,
-                imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-                imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-                name: 'Fluffy',
-                sex: 'Female',
-                age: 2,
-                breed: 'Bengal',
-                story: 'Thrown on the street'
-                },{id:2,
-                    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-                    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-                    name: 'Fluffy2',
-                    sex: 'Female',
-                    age: 2,
-                    breed: 'Bengal',
-                    story: 'Thrown on the street'
-                    }]
+            first: [],
+            display: []
         },
         Cats: {
-            first: [{ id: 1,
-                imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-                imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-                name: 'Fluffy5',
-                sex: 'Female',
-                age: 2,
-                breed: 'Bengal',
-                story: 'Thrown on the street'
-                }],
-            list: [{ id: 2,
-                imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-                imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-                name: 'Fluffy6',
-                sex: 'Female',
-                age: 2,
-                breed: 'Bengal',
-                story: 'Thrown on the street'
-                },
-                { id: 1,
-                    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-                    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-                    name: 'Fluffy7',
-                    sex: 'Female',
-                    age: 2,
-                    breed: 'Bengal',
-                    story: 'Thrown on the street'
-                    }]
-                }
+            first: [],
+            list: []
+        }
     }
     setDogfirst = (data) =>{
        const firstDog = [data]
