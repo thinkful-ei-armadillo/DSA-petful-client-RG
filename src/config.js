cf62b69842e8f8aacba5572b905f8b568d11522b
Robin Khiv <robinkhiv@gmail.com>
2019-05-19 23:37:44 -0700

    changed api endpoint for server

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://powerful-hamlet-41307.herokuapp.com/api'
+    API_ENDPOINT: 'https://frozen-ridge-30764.herokuapp.com/api'
+    //API_ENDPOINT: 'https://powerful-hamlet-41307.herokuapp.com/api'
 }
